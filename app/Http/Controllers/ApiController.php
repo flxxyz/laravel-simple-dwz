3f045668e8cae39779c2b347340cc75633d16ce0
flxxyz <i@mdzz.name>
2017-07-08 00:52:27 +0800

    add top_new

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\TopNew;
 use Illuminate\Http\Request;
 
 use App\Util;
@@ -17,7 +18,8 @@ class ApiController extends Controller
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function produce(Request $request) {
+    public function produce(Request $request)
+    {
         // 构造回调json
         $response = [
             'data' => [
@@ -29,14 +31,14 @@ class ApiController extends Controller
             'message' => ''
         ];
 
-        if($request->isMethod('POST')) {
+        if ($request->isMethod('POST')) {
             $uri = $request->get('uri');
             $hash = base_convert(abs(crc32($uri)), 10, 32);
             $ip = Util::getClientIP();
 
             // 存在hash返回已有hash信息
             $link = Link::where('hash', $hash)->get();
-            if(count($link) != 0) {
+            if (count($link) != 0) {
                 Show::find($link[0]->id)->increment('produce');  // 创建+1
                 $response['statu'] = 200;
                 $response['message'] = 'hash exist';
@@ -53,10 +55,28 @@ class ApiController extends Controller
             if ($row = Link::create($data)) {
                 //dd($row);
                 $show = new Show();
-                if($id = $show->insertGetId(['link_id' => $row->id])) {
+                if ($id = $show->insertGetId(['link_id' => $row->id])) {
                     $show->where('id', $id)->increment('produce');
                 }
-                $response['data']['uri'] = Util::getHost() . $row->hash;
+
+                $uri = $row->uri;
+                $hash = Util::getHost() . $row->hash;
+                ob_start();
+                $html = Util::getHttp($uri);
+                ob_end_clean();
+                $htmlDom = new Htmldom();
+                $htmlDom->load($html);
+                $title = $htmlDom->find('title', 0)->innertext;
+
+                if (empty($title)) {
+                    $title = $hash;
+                }
+
+                // 添加至最新排行表
+                $data = ['link_id' => $row->id, 'title' => $title];
+                TopNew::create($data);
+
+                $response['data']['uri'] = $hash;
                 $response['statu'] = 200;
                 $response['message'] = 'success';
                 $response['data']['created_at'] = $row->created_at;
@@ -66,7 +86,7 @@ class ApiController extends Controller
                 $response['message'] = 'error';
                 return response()->json($response);
             }
-        }else {
+        } else {
             $response['data']['uri'] = Util::getHost();
             return response()->json($response);
         }
@@ -84,28 +104,21 @@ class ApiController extends Controller
                 'query_time' => time()
             ]
         ];
-        $link = Link::orderBy('id', 'desc')->get();
-        $count = count($link);
+
+        $topNew = TopNew::orderBy('id', 'desc')->take(10)->get();
+        $count = count($topNew);
 
         if ($count != 0) {
-            foreach ($link as $key => $item) {
-                $uri = $item->uri;
-                ob_start();
-                $html = Util::getHttp($uri);
-                ob_end_clean();
-                $htmlDom = new Htmldom();
-                $htmlDom->load($html);
-                $title = $htmlDom->find('title', 0)->innertext;
+            foreach ($topNew as $key => $item) {
+                $title = $item->title;
 
-                if (empty($title)) {
-                    $title = $uri;
-                }
                 $data['result']['data'][] = [
                     'created_at' => $item->created_at->toDateTimeString(),
                     'title' => $title,
-                    'uri' => Util::getHost() . $item->hash
+                    'uri' => Util::getHost() . Link::find($item->link_id)->hash
                 ];
             }
+            $data['statu'] = 200;
             $data['result']['count'] = $count;
             $data['message'] = 'Query Success';
             return response()->json($data);
